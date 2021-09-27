344c238d59ff0dddf0a97da93f79430504a50ae2
AntonMatyash <90440031+AntonMatyash@users.noreply.github.com>
2021-09-27 22:46:23 +0300

    commit

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z1
+{
+    class Address
+
+    {
+
+        public string index; // индекс
+
+        public string country;     // Страна
+
+        public string city;     // город
+
+        public string street;     // улица
+
+        public int house;     //номер улицы
+
+        public int apartment;     // квартира
+
+        public void GetInfo()
+
+        {
+
+            Console.WriteLine($"index: {index} \ncountry: {country} \ncity: {city} \nstreet: {street} \nhouse: {house} \napartment: {apartment}");
+
+        }
+
+    }
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Address home = new Address();
+
+            home.index = "03500";
+
+            home.country = "Ukraine";
+
+            home.city = "Baryshivka";
+
+            home.street = "Zentralna";
+
+            home.house = 20;
+
+            home.apartment = 6;
+
+            home.GetInfo();
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
---
+++ Program5.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Z5
+{
+    class User
+
+    {
+
+        public string login; // индекс
+
+        public string name;     // Страна
+
+        public string surname;     // город
+
+        public string age;     // улица
+
+        private DateTime date = DateTime.Today;
+
+
+
+
+
+        public void GetInfo()
+
+        {
+            Console.WriteLine($"login: {login} \nname: {name} \nsurname: {surname} \nage: {age} \ndate of registrarion: {date.ToString("D")}\n");
+
+        }
+
+    }
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            User user = new User();
+
+            user.login = "a.matyashr@gmail.com";
+
+            user.name = "Anton";
+
+            user.surname = "Matyash";
+
+            user.age = "18";
+
+            user.GetInfo();
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
